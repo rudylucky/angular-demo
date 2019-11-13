471afd04568dae21f576f2be3e5b791682556e55
rudylucky <1284582372@qq.com>
2019-11-13 14:58:08 +0000

    udpate route add role-info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from '@/system/components/welcome/welcome.component';
 import { UserInfoComponent } from '@/system/components/user-info/user-info.component';
+import { RoleInfoComponent } from '@/system/components/role-info/role-info.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/user-info' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'user-info', component: UserInfoComponent },
+  { path: 'role-info', component: RoleInfoComponent },
 ];
 
 @NgModule({
