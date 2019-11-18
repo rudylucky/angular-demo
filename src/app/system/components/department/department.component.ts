6f62137032fe3e70adc1d7a29f99cff705eb3cad
rudylucky <1284582372@qq.com>
2019-11-18 15:40:00 +0000

    tree demo

--- src/app/system/components/department/department.component.ts
+++ src/app/system/components/department/department.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { NzFormatBeforeDropEvent } from 'ng-zorro-antd';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-department',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentComponent implements OnInit {
 
+  nodes = [
+    {
+      title: '0-0',
+      key: '100',
+      expanded: true,
+      children: [
+        {
+          title: '0-0-0',
+          key: '1001',
+          children: [{ title: '0-0-0-0', key: '10010', isLeaf: true }, { title: '0-0-0-1', key: '10011', isLeaf: true }]
+        },
+        {
+          title: '0-0-1',
+          key: '1002',
+          children: [{ title: '0-0-1-0', key: '10020', isLeaf: true }]
+        }
+      ]
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  beforeDrop(arg: NzFormatBeforeDropEvent): Observable<boolean> {
+    // if insert node into another node, wait 1s
+    if (arg.pos === 0) {
+      return of(true).pipe(delay(1000));
+    } else {
+      return of(false);
+    }
   }
 
 }
--- src/app/system/components/welcome/welcome.component.ts
+++ src/app/system/components/welcome/welcome.component.ts
@@ -1,29 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NzFormatBeforeDropEvent } from 'ng-zorro-antd';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
+  template: ` <nz-tree [nzData]="nodes" nzDraggable nzBlockNode [nzBeforeDrop]="beforeDrop"> </nz-tree> `,
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
 
-  validateForm: FormGroup;
+  nodes = [
+    {
+      title: '0-0',
+      key: '100',
+      expanded: true,
+      children: [
+        {
+          title: '0-0-0',
+          key: '1001',
+          children: [{ title: '0-0-0-0', key: '10010', isLeaf: true }, { title: '0-0-0-1', key: '10011', isLeaf: true }]
+        },
+        {
+          title: '0-0-1',
+          key: '1002',
+          children: [{ title: '0-0-1-0', key: '10020', isLeaf: true }]
+        }
+      ]
+    }
+  ];
 
-  constructor(private fb: FormBuilder) { }
+  public ngOnInit() {
 
-  ngOnInit(): void {
-    this.validateForm = this.fb.group({
-      userName: [null, [Validators.required]],
-      remember: [true]
-    });
   }
 
-  submitForm(): void {
-    for (const i in this.validateForm.controls) {
-      this.validateForm.controls[i].markAsDirty();
-      this.validateForm.controls[i].updateValueAndValidity();
+  beforeDrop(arg: NzFormatBeforeDropEvent): Observable<boolean> {
+    // if insert node into another node, wait 1s
+    if (arg.pos === 0) {
+      return of(true).pipe(delay(1000));
+    } else {
+      return of(false);
     }
   }
+
 
 }
